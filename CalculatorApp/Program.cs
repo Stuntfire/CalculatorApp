461b04bc1a2ff583a25b8dc037007744c0e7f51b
Søren Roed Schack <soer622t@edu.easj.dk>
2017-09-13 08:57:32 +0200

    Lommeregner med 4 funktioner.

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SharedCalculatorLibrary;
 
 namespace CalculatorApp
 {
@@ -10,6 +11,38 @@ namespace CalculatorApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("-------------- Velkommen til din nye lommeregner --------------");
+
+            Console.WriteLine("Indtast dit første tal...");
+            string Tal1 = Console.ReadLine();
+            double temp1 = Double.Parse(Tal1);
+
+            Console.WriteLine("Indtast en operator...");
+            string Operator = Console.ReadLine();
+
+            Console.WriteLine("Indtast dit andet tal...");
+            string Tal2 = Console.ReadLine();
+            double temp2 = Double.Parse(Tal2);
+
+            var calc = new BasicOperations(temp1, temp2);
+            switch (Operator)
+            {
+                case "+":
+                    Console.WriteLine(calc.PlusFunktion());
+                    break;
+                case "-":
+                    Console.WriteLine(calc.MinusFunktion());
+                    break;
+                case "*":
+                    Console.WriteLine(calc.GangeFunktion());
+                    break;
+                case "/":
+                    Console.WriteLine(calc.DividerFunktion());
+                    break;
+                default:
+                    Console.WriteLine("Indtast venligst en korrekt operator.");
+                    break;
+            }
         }
     }
 }
